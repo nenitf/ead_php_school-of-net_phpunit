fafd6bc1a446135ccfc02de43cc0455d632aace9
Felipe Silva <felipe.dacs@gmail.com>
2019-10-18 23:32:34 -0300

    release: add aula 20

---
+++ tests/ProductDBTest.php
@@ -0,0 +1,110 @@
+<?php
+
+class ProductDBTest extends PHPUnit\Framework\TestCase
+{
+
+    private $db;
+
+    protected function setUp():void
+    {
+        $this->db = getPDO();
+    }
+
+    public function testIfProductIsSaved()
+    {
+        $result = $this->createProduct();
+
+        $this->assertEquals(1, $result->getId());
+        $this->assertEquals('Produto 1', $result->getName());
+        $this->assertEquals(200.20, $result->getPrice());
+        $this->assertEquals(10, $result->getQuantity());
+        $this->assertEquals(200.20 * 10, $result->getTotal());
+        return $result->getId();
+    }
+
+    public function testIfListProducts(){
+        $db = $this->db;
+        $product = new \App\Product($db);
+        $this->createProduct();
+        $this->createProduct();
+
+        $products = $product->all();
+        $this->assertCount(2, $products);
+    }
+
+    /**
+     * @depends testIfProductIsSaved
+     */
+    public function testIfProductIsUpdated($id)
+    {
+        $db = $this->db;
+        $product = new \App\Product($db);
+        $result = $product->save([
+            'id' => $id,
+            'name' => 'Produto Alterado',
+            'price' => 300.20,
+            'quantity' => 20
+        ]);
+
+        $this->assertEquals($id, $result->getId());
+        $this->assertEquals('Produto Alterado', $result->getName());
+        $this->assertEquals(300.20, $result->getPrice());
+        $this->assertEquals(20, $result->getQuantity());
+        $this->assertEquals(300.20 * 20, $result->getTotal());
+        return $id;
+    }
+
+    /*
+     * @depends testIfProductIsUpdated
+     */
+    //public function testIfProductCanBeRecovered($id)
+    public function testIfProductCanBeRecovered()
+    {
+        $this->createProduct();
+        $db = $this->db;
+        $product = new \App\Product($db);
+        $result = $product->find(1);
+
+        $this->assertEquals(1, $result->getId());
+        $this->assertEquals('Produto 1', $result->getName());
+        $this->assertEquals(200.20, $result->getPrice());
+        $this->assertEquals(10, $result->getQuantity());
+        $this->assertEquals(200.20 * 10, $result->getTotal());
+    }
+
+    /*
+     * @depends testIfProductIsUpdated
+     */
+    //public function testIfProductCanDeleted($id)
+    public function testIfProductCanDeleted()
+    {
+        $this->createProduct();
+        $db = $this->db;
+        $product = new \App\Product($db);
+        $result = $product->delete(1);
+
+        $this->assertTrue($result);
+        $products = $product->all();
+        $this->assertCount(0, $products);
+    }
+
+    public function testIfProductNotFound()
+    {
+        $db = $this->db;
+        $product = new \App\Product($db);
+        $this->expectException($product->find(99999));
+
+                $this->expectExceptionMessage('Exception: Produto não existe.');
+    }
+
+    private function createProduct(){
+        $db = $this->db;
+        $product = new \App\Product($db);
+        return $product->save([
+            'name' => 'Produto 1',
+            'price' => 200.20,
+            'quantity' => 10
+        ]);
+    }
+}
+
---
+++ tests/ProductTest.php
@@ -0,0 +1,58 @@
+<?php
+
+class ProductTest extends PHPUnit\Framework\TestCase
+{
+
+    private $product;
+
+    protected function setUp():void
+    {
+        $pdo = $this->getMockBuilder(\PDO::class)
+            ->disableOriginalConstructor()->getMock();
+        $this->product = new \App\Product($pdo);
+    }
+
+    public function testIfIdIsZero()
+    {
+        $this->assertEquals(0,$this->product->getId());
+    }
+
+    public function testIfTotalIsZero()
+    {
+        $this->assertEquals(0.0,$this->product->getTotal());
+    }
+
+    /**
+     * @dataProvider collectionData
+     */
+    public function testEncapsulate($property, $expected)
+    {
+
+        $null = $this->product->{'get' . ucfirst($property)}();
+        if(!is_float($expected) && !is_int($expected)) {
+            $this->assertNull($null);
+        }else{
+            if(is_float($expected)){
+                $this->assertEquals(0.0, $null);
+            }else{
+                $this->assertEquals(0, $null);
+            }
+        }
+
+        $result = $this->product->{'set' . ucfirst($property)}($expected);
+        $this->assertInstanceOf(\App\Product::class, $result);
+
+        $actual = $this->product->{'get' . ucfirst($property)}();
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function collectionData()
+    {
+        return [
+            ['name', 'Produto 1'],
+            ['price', 10.11],
+            ['quantity', 5],
+        ];
+    }
+}
+
